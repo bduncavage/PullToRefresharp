331bb66461507b38aa5e8ca8f83290e99585012d
Brett Duncavage <brett.duncavage@rd.io>
2013-06-20 15:02:23 -0700

    Cleanup sample layouts a bit, fix ListPadding bug for < HoneyComb
    The child's Top includes the ListPaddingTop on < HoneyComb. I have fixed this by
    checking the ListPaddingTop value if the first child is visible and its Top != 0.

--- PullToRefresharp.Android/PullToRefresharp.Delegates/ListViewDelegate.cs
+++ PullToRefresharp.Android/PullToRefresharp.Delegates/ListViewDelegate.cs
@@ -1,6 +1,7 @@
 using System;
 using Android.Views;
 using Android.Widget;
+using Android.OS;
 
 using PullToRefresharp.Android.Views;
 
@@ -15,7 +16,17 @@ namespace PullToRefresharp.Android.Delegates
         public override bool IsAtTop {
             get {
                 var child = view.GetChildAt(0);
-                return view.FirstVisiblePosition == 0 && (child == null || child.Top == 0);
+                var is_child_at_top = view.FirstVisiblePosition == 0 && (child == null || child.Top == 0);
+
+                if (child != null && !is_child_at_top && view.FirstVisiblePosition == 0) {
+                    // If you build with a sane Android SDK version this is not necessary.
+                    // But older Android versions count the View's ListPaddingTop as part of Top.
+                    // we have to take into account padding/margin
+
+                    // make sure we offset for the padding.
+                    is_child_at_top = child.Top - view.ListPaddingTop == 0;
+                }
+                return is_child_at_top;
             }
         }
     }
